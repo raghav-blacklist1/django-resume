77ab28c6610eb162a1ee9df72cd009bb1eb100cf
Raghav Sangar <31736355+raghav-blacklist1@users.noreply.github.com>
2020-05-23 18:35:00 +0530

    Update settings.py

--- resume/settings.py
+++ resume/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
